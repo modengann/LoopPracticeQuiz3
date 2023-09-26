924bd246e95977ea5a29a6622f0f5bab0d95e52b
modengann <99417989+modengann@users.noreply.github.com>
2023-09-25 21:11:45 -0400

    init

---
+++ src/App.java
@@ -0,0 +1,15 @@
+/**
+ *  Write code that reads a series of test scores (as integers) from the user until the user 
+ *  enters -1. The test scores should be in a range from 0 to 100. If the user enters an invalid number
+ *  tell them to re-enter the value. Keep track of the number of passing and failing test grades. Assume 
+ *  that a failing grade is below a 60. When the user enters '-1', print out the number of passing and failing students.
+ */
+
+
+ import java.util.*;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner(System.in);
+    }
+}
